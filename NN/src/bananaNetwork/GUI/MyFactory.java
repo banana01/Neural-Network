f794514a24813ff792b16040a514ee65b6b1bcca
100102981(Miles Yoshinobu_School) <milesyoshinobu@gmail.com>
2016-01-25 16:36:19 -0600

    Worked on TICTACTOE gui created new button type.
    Simplifing and making code more readable. Still having problems with
    init location and imgs trying to init before buttons have been made.

--- NN/src/bananaNetwork/GUI/MyFactory.java
+++ NN/src/bananaNetwork/GUI/MyFactory.java
@@ -1,6 +1,8 @@
 package bananaNetwork.GUI;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.IOException;
+import java.nio.file.Path;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -18,10 +20,11 @@ public final class MyFactory {
 		return button;
 	}
 	/**
+	 * @throws IOException 
 	 * @wbp.factory
 	 */
-	public static JButton createBoardPosButton(ImageIcon t) {
-		JButton button = new JButton(t);
+	public static TTTButton createBoardPosButton(int t, Path e, Path o, Path x) throws IOException {
+		TTTButton button = new TTTButton(t, e, o, x);
 		return button;
 	}
 }
--- NN/src/bananaNetwork/GUI/TicTacToeBoard.java
+++ NN/src/bananaNetwork/GUI/TicTacToeBoard.java
@@ -26,59 +26,69 @@ public class TicTacToeBoard extends JPanel implements ActionListener
 	 * Create the panel.
 	 */
 	private int temp1;
-	JButton[] buttons;
+	TTTButton[] buttons;
 	int[] board = {0,0,0,0,0,0,0,0,0};
 	BufferedImage imgx, imgo, imgb;
     ImageIcon iconx, icono, iconb;
     JLabel Turnlabel;
     boolean turn = true;
-	public TicTacToeBoard(Path x, Path o, Path empty) throws IOException 
+    int buW, buH;
+    Path x,o,e;
+	public TicTacToeBoard(Path empty, Path o, Path x) throws IOException 
 	{
-		imgx = ImageIO.read(new File(x.toString()));
-		iconx = new ImageIcon(imgx.getScaledInstance(128, 128, Image.SCALE_SMOOTH));
-		imgo = ImageIO.read(new File(o.toString()));
-		icono = new ImageIcon(imgo.getScaledInstance(128, 128, Image.SCALE_SMOOTH));
-		imgb = ImageIO.read(new File(empty.toString()));
-		iconb = new ImageIcon(imgb.getScaledInstance(128, 128, Image.SCALE_SMOOTH));
+		setXi(x);
+		setO(o);
+		setE(empty);
+		buttons = new TTTButton[9];
 		setLayout(new BorderLayout(0, 0));
-		
-		Turnlabel = new JLabel("true");
+		Turnlabel = new JLabel("X's Turn!");
 		add(Turnlabel, BorderLayout.NORTH);
-		
 		JPanel gamepanel = new JPanel();
 		add(gamepanel, BorderLayout.CENTER);
 		gamepanel.setLayout(new GridLayout(3, 3, 0, 0));
-		buttons = new JButton[9];
+		
 		for (int i = 0; i < buttons.length; i++) 
 		{
-			buttons[i] = MyFactory.createBoardPosButton(iconb);
+			buttons[i] = MyFactory.createBoardPosButton(0, empty, o, x);
 			buttons[i].setName(""+i);
 			gamepanel.add(buttons[i]);
 			buttons[i].addActionListener(this);
 		}
-		//repaint();
+		//for(int i = 0; i < buttons.length; i++)
+		//{
+		//	buttons[i].initIcons();
+		//}
+		System.out.println( buttons[5].getSize());
+		
 		
 	}
-	public void updateButtons()
-	{
-		for (temp1 = 0; temp1 < buttons.length; temp1++) 
-		{
-			//System.out.println(temp1);
-			if(board[temp1] == 1)
-			{
-				buttons[temp1].setIcon(iconx);
-			}
-			else if(board[temp1] == -1)
-			{
-				buttons[temp1].setIcon(icono);
-			}
-			else
-			{
-				buttons[temp1].setIcon(iconb);
-			}
-			
-		}
+	
+	
+	
+	
+	
+	public Path getXi() {
+		return x;
 	}
+	public void setXi(Path x) {
+		this.x = x;
+	}
+	public Path getO() {
+		return o;
+	}
+	public void setO(Path o) {
+		this.o = o;
+	}
+	public Path getE() {
+		return e;
+	}
+	public void setE(Path e) {
+		this.e = e;
+	}
+	
+	
+	
+	
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
@@ -86,15 +96,13 @@ public class TicTacToeBoard extends JPanel implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
-		JButton temp = (JButton) e.getSource();
+		TTTButton temp = (TTTButton) e.getSource();
 		if(turn == true)
 		{
 			if(modifyBoard(Integer.parseInt(temp.getName()), 1))
 			{
 				turn = false;
 			}
-			
-			
 		}
 		else
 		{
@@ -105,8 +113,14 @@ public class TicTacToeBoard extends JPanel implements ActionListener
 			
 			
 		}
-		Turnlabel.setText(Boolean.toString(turn));
-		updateButtons();
+		if(turn == true)
+		{
+			Turnlabel.setText("X's Turn!");
+		}
+		else
+		{
+			Turnlabel.setText("O's Turn!");
+		}
 		repaint();
 
 	}
