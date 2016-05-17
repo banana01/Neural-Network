ef04a83f10256abf0de583757b031efda3559d37
100102981(Miles Yoshinobu_School) <milesyoshinobu@gmail.com>
2016-05-17 16:16:31 -0500

    GUI has reached a point where i have no idea what im doing
    Gui dosent really make sense need to read up on, will go back to
    internals for no due to bug i dont understand and my poor gui
    programming

--- NN/src/bananaNetwork/GUI/ConnectionsLayer.java
+++ NN/src/bananaNetwork/GUI/ConnectionsLayer.java
@@ -45,23 +45,31 @@ public class ConnectionsLayer extends LayerUI<JComponent>
        // g.drawLine(5, 500, 500, 500);
         Graphics2D g2d = (Graphics2D)g;
         //g.setPaint ( Color.BLACK );
-        g2d.setStroke(new BasicStroke(15));
+        g2d.setStroke(new BasicStroke(5));
         g2d.setColor(Color.BLACK);
         // TODO: WTF IS GOING ON HERE IT WONT GO THROUGH THE ITORATOR AND I HAVE NO IDEA WY IT WONT DO THE WHILE LOOP NORE A FOR LOOP
-        
-        Iterator it = links.entrySet().iterator();
-        while (it.hasNext()) {
+       // System.out.println(links.entrySet());
+       // Iterator it = links.entrySet().iterator();
+        //System.out.println(it.hasNext());
+        /*while (it.hasNext()) {
         	Map.Entry pair = (Map.Entry)it.next();
         	System.out.println(pair.getKey());
-        	System.out.println("YOLOSWAG");
             Point p1 = (Point) pair.getKey();
             Point p2 = (Point) pair.getValue();
             g2d.drawLine( (int)p1.getX(), (int)p1.getY(), (int)p2.getX(), (int)p2.getY() );
-            g2d.drawLine(5, 5, 500, 500);
-            System.out.println("YOLOSWAG");
+            System.out.println("DEBUG2");
+        }*/
+        //links.put(new Point(50, 500), new Point(50, 500));
+        System.out.println(links.keySet());
+        for(Point e:links.keySet())
+        {
+        	Point p1 = e;
+            Point p2 = links.get(e);
+            System.out.println(p1+"TOTATO");
         }
-    }
-
+        //drawConnection(g, new Point(500, 50));
+       
+	}
     public void installUI(JComponent c) {
         super.installUI(c);
         // enable mouse motion events for the layer's subcomponents
@@ -88,16 +96,19 @@ public class ConnectionsLayer extends LayerUI<JComponent>
     		last = null;
     		System.out.println(links.values());
     	}
+    	
     }
     public void link(Point p1, Point p2)
     {
     	links.put(p1, p2);
+    	System.out.println("THIS HAPPENDED");
+    	
     	
     }
-    public void drawConnection(Graphics g, GUINode GN)
+    public void drawConnection(Graphics g, Point GN)
     {
     	Point p = MouseInfo.getPointerInfo().getLocation();
-    	g.drawLine(GN.getX(),GN.getY(),(int)Math.ceil(p.getX()),(int)Math.ceil(p.getY()));
+    	g.drawLine((int)GN.getX(),(int)GN.getY(),(int)Math.ceil(p.getX()),(int)Math.ceil(p.getY()));
     }
     
     
